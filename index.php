93f0df6f2bac436aeb48915a684775402e953b89
David McDonald <idavidmcdonald@me.com>
2015-06-06 16:29:07 +0100

    Added GET, POST, PUT and DELETE endpoints

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 
-// Configure DB
+// Configure ORM connection
 ORM::configure(array(
     'connection_string' => 'mysql:host=localhost;dbname=blog',
     'username' => 'root',
@@ -15,14 +15,30 @@ class MissingArgumentsException extends Exception {};
 
 $app = new \Slim\Slim();
 
-// GET all blogs
+/**
+ * GET request to retrive all blog
+ *
+ * 200 on success
+ * 500 on other failure
+ */
 $app->get('/blogs/', function () use ($app) {
-	$blogs = ORM::for_table('blogs')->find_array();
-	$app->response()->header('Content-Type', 'application/json');
-	echo json_encode($blogs);
+	try {
+		$blogs = ORM::for_table('blogs')->find_array();
+		$app->response()->header('Content-Type', 'application/json');
+		echo json_encode($blogs);
+	} catch (Exception $e) {
+		$app->response()->status(500);
+    	$app->response()->header('X-Status-Reason', $e->getMessage());
+	}	
 });
 
-// GET blog by ID
+/**
+ * GET request to retrive a blog
+ *
+ * 200 on success
+ * 404 on resource not found
+ * 500 on other failure
+ */
 $app->get('/blogs/:id/', function ($id) use ($app) {
 	try {
 		$blog = ORM::for_table('blogs')->find_one($id);
@@ -42,15 +58,22 @@ $app->get('/blogs/:id/', function ($id) use ($app) {
 	
 });
 
-// POST request to insert blog
+/**
+ * POST request to insert a blog
+ *
+ * Requires "title" and "content" inputs. 
+ * Blog datetime_submitted will be set to current datetime
+ *
+ * 201 on success
+ * 400 on missing arguments
+ * 500 on other failure
+ */
 $app->post('/blogs/', function () use ($app) {    
     try {
-	    // Get and decode JSON request body
 	    $request = $app->request();
 	    $body = $request->getBody();
 	    $input = json_decode($body); 
 	    
-	    // If title and content provided, then save to DB
 	    if (isset($input->title) AND isset($input->content)) {
 	    	$newBlog = ORM::for_table('blogs')->create();
 	    	$newBlog->set(array(
@@ -68,6 +91,76 @@ $app->post('/blogs/', function () use ($app) {
 	    }
 	} catch (MissingArgumentsException $e) {
 		$app->response()->status(400);
+	} catch (Exception $e) {
+	    $app->response()->status(500);
+	    $app->response()->header('X-Status-Reason', $e->getMessage());
+	}
+});
+
+/**
+ * PUT request to update a blog
+ *
+ * Requires "title", "content" and "datetime_submitted" inputs. 
+ *
+ * 200 on success
+ * 400 on missing arguments
+ * 404 on resource not found
+ * 500 on other failure
+ */
+$app->put('/blogs/:id/', function ($id) use ($app) {      
+    try {
+    	$blog = ORM::for_table('blogs')->find_one($id);
+
+    	if ($blog) {
+		    $request = $app->request();
+		    $body = $request->getBody();
+		    $input = json_decode($body); 
+
+		    if (isset($input->title) AND isset($input->content) AND isset($input->datetime_submitted)) {
+		    	$blog->set(array(
+					'title'              => $input->title,
+					'content'            => $input->content,
+					'datetime_submitted' => $input->datetime_submitted
+				));
+				$blog->save();
+		    } else {
+		    	throw new MissingArgumentsException();
+		    }
+
+			$app->response()->header('Content-Type', 'application/json');
+	    	echo json_encode($blog->as_array());
+    	} else {
+    		throw new ResourceNotFoundException();
+    	}
+	} catch (ResourceNotFoundException $e) {
+		$app->response()->status(404);
+	} catch (MissingArgumentsException $e) {
+		$app->response()->status(400);
+	} catch (Exception $e) {
+	    $app->response()->status(500);
+	    $app->response()->header('X-Status-Reason', $e->getMessage());
+	}
+});
+
+/**
+ * DELETE request to delete a blog
+ *
+ * 204 on success
+ * 404 on resource not found
+ * 500 on other failure
+ */
+$app->delete('/blogs/:id', function ($id) use ($app) {    
+  	try {
+	    $blog = ORM::for_table('blogs')->find_one($id);
+	    
+	    if ($blog) {
+	      	$blog->delete();
+	      	$app->response()->status(204);
+	    } else {
+	      	throw new ResourceNotFoundException();
+	    }
+  	} catch (ResourceNotFoundException $e) {
+    	$app->response()->status(404);
 	} catch (Exception $e) {
 	    $app->response()->status(500);
 	    $app->response()->header('X-Status-Reason', $e->getMessage());

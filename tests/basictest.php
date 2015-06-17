eaedcc066d1c94e568b62c42edf8ddb730724781
David McDonald <idavidmcdonald@me.com>
2015-06-17 22:49:08 +0100

    added phpunit and travis test

---
+++ tests/basictest.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\tests;
+class BasicTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+	{
+	    $foo = true;
+	    $this->assertTrue($foo);
+	}
+}
